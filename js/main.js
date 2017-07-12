9ab157034ee01084c78b9d378414259137edda6b
Delahaye Marien <delahaye.marien@gmail.com>
2017-07-12 15:39:39 +0200

    Added more codes in main.js

--- js/main.js
+++ js/main.js
@@ -1,23 +1,37 @@
 // Variables //
-var ul= [];
+var ulImages = document.getElementsByTagName("img");
 var liItems; 
 var imageWidth;
-var imageNumber;
+var imageNumber = ulImages.length;
+var laps = 0;
+var timer;
 
 // Functions if needed //
+function hideImages(){
+    for (var i = 0; i < imageNumber; i++){
+        if (i === laps){
+           ulImages[i].style.display = "block"; 
+        }
+        else{
+            ulImages[i].style.display = "none";
+        }
 
+    }
+}
+function timerInterval() {
+    timer = setInterval(slider, 3000);
+}
+function slider() {
+    
+}
 // Script part //
-{
-ul = document.getElementsByTagName("img");
 // liItems = ul.children;
-imageNumber = ul.length;
-for (var i = 0; i < ul.length; i++){
-
-
-}
-
+hideImages();
+timerInterval();
+slider();
 
 
 
 
-}
+
+
