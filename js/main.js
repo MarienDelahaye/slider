95dc548fe08edcb4e8fa288fdcfa9ff7ce8413e5
Delahaye Marien <delahaye.marien@gmail.com>
2017-07-13 16:51:52 +0200

    Final edit on main.js

--- js/main.js
+++ js/main.js
@@ -1,33 +1,23 @@
 // Variables //
 var ulImages = document.getElementsByTagName("img");
-var liItems; 
-var imageWidth;
 var imageNumber = ulImages.length;
 var laps = 0;
-var timer;
+var i;
 
 // Functions if needed //
 function hideImages(){
-    for (var i = 0; i < imageNumber; i++){
-        if (i === laps){
-           ulImages[i].style.display = "block"; 
-        }
-        else{
-            ulImages[i].style.display = "none";
-        }
-
+    for (let i = 0; i < imageNumber; i++)
+        { ulImages[i].style.display = "none";
     }
+    laps++;
+if (laps == ulImages.length) {laps = 1}
+ulImages[laps-1].style.display = "block";
+setTimeout(hideImages, 500);
+
 }
-function timerInterval() {
-    timer = setInterval(launchTimer, 9000);
-}
-function launchTimer() {
-    alert("Test");
-}
+
 // Script part //
 hideImages();
-launchTimer();
-timerInterval();
 
 
 
