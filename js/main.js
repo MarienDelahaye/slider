c2fc45cd12b45c4f43fcd77714167c1744e8e387
Delahaye Marien <delahaye.marien@gmail.com>
2017-07-17 09:07:44 +0200

    Commented my main.js

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
 // Variables //
-var ulImages = document.getElementsByTagName("img");
-var imageNumber = ulImages.length;
-var laps = 0;
-var i;
+var ulImages = document.getElementsByTagName("img"); // Récupère les images sous forme de tableau
+var imageNumber = ulImages.length; // Nombre d'image dans le tableau
+var laps = 0; // Nombre de tour pour la boucle for
+var i; // Variables utiliser pour la boucle for
 
 // Functions if needed //
-function hideImages(){
+function hideImages(){ // Fonction pour cacher les images pour ensuite les afficher une par une ( pour le slider)
     for (let i = 0; i < imageNumber; i++)
         { ulImages[i].style.display = "none";
     }
     laps++;
 if (laps == ulImages.length) {laps = 1}
-ulImages[laps-1].style.display = "block";
+ulImages[laps-1].style.display = "block"; // Conditions pour le slider
 setTimeout(hideImages, 500);
 
 }
 
 // Script part //
-hideImages();
+hideImages(); // Execute la fonction hideImages
 
 
 
